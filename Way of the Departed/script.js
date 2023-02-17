7b437aeb1cd32836fa81d335178d15dbaffd6f68
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-02-17 16:49:00 -0300

    Organization changes

--- Way of the Departed/script.js
+++ Way of the Departed/script.js
@@ -1,36 +1,31 @@
-/* Set the width of the side navigation to 200px */
+/* Set the width of the side navigation to 155px */
 function openNav() {
-  document.getElementById('sidenav').style.width = '155px';
+	document.getElementById('sidenav').style.width = '155px';
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById('sidenav').style.width = '0';
+	document.getElementById('sidenav').style.width = '0';
 }
 
 /* Change the content of the page according to the selected chapter */
 function change(T, I, N) {
 
 	/* Change the page title */
- document.getElementById('tit').innerHTML = `Chapter ${N}`;
+	document.getElementById('title').innerHTML = `Chapter ${N}`;
 
 	/* Change the text */
- document.getElementById('text').innerHTML = T;
+	document.getElementById('text').innerHTML = T;
 
 	/* Change the image */
- document.getElementById('im').src = `imgs/${N}.jpg`;
+	document.getElementById('im').src = `imgs/${N}.jpg`;
 
 	/* Change the image alt text*/
- document.getElementById('im').alt= I;
+	document.getElementById('im').alt= I;
 
 	/* Closes back the navigation bar */
-closeNav();
+	closeNav();
 
 	/* Takes the reader back to the top */
- up();
-}
-
-/* Scrolls back to the top of the page */
-function up() {
- document.documentElement.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
